7468728531118dc697b23f9bf7a9239559539005
SubtotalCamp875 <jadenzh2020@gmail.com>
2024-06-09 22:15:38 -0400

    Initial commit

---
+++ src/main/java/net/subtotalcamp875/vermidamod/VermidaModFabric.java
@@ -0,0 +1,16 @@
+package net.subtotalcamp875.vermidamod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VermidaModFabric implements ModInitializer {
+	public static final String MOD_ID = "vermida_mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/subtotalcamp875/vermidamod/VermidaModFabricClient.java
@@ -0,0 +1,10 @@
+package net.subtotalcamp875.vermidamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VermidaModFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
